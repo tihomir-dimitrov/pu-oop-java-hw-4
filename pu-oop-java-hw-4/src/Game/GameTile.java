657b242109f50cee56aa3d859226c4c37a7a1e10
tihomir-dimitrov <63392523+tihomir-dimitrov@users.noreply.github.com>
2021-02-09 15:48:01 +0200

    Update GameTile.java

--- pu-oop-java-hw-4/src/Game/GameTile.java
+++ pu-oop-java-hw-4/src/Game/GameTile.java
@@ -2,44 +2,60 @@ package Game;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
+
+import static java.awt.Color.*;
 
 public class GameTile extends JFrame {
+    public static final int FIELD_SIZE = 100;
     private int row;
     private int col;
-    private static final int FIELD_SIZE=100;
 
-    public GameTile(int row, int col){
+    public GameTile(int row, int col) {
         this.row = row;
         this.col = col;
     }
 
-    public void backGroundColor(Graphics g){
-        setBackground(Color.PINK);
+    public void render(Graphics g) {
+        boardGrid(g);
     }
 
-    public void greenTile(Graphics g){
-        g.setColor(Color.GREEN);
-        g.fillRect(600,0,100,100);
-        g.fillRect(100,100,100,100);
-        g.fillRect(500,200,100,100);
-        g.fillRect(100,300,100,100);
-        g.fillRect(300,300,100,100);
-        g.fillRect(600,400,100,100);
-        g.fillRect(200,500,100,100);
-        g.fillRect(600,700,100,100);
+    void boardGrid(Graphics g) {
+        g.setColor(Color.black);
+        for (int i = 0; i <= 700; i += 100) {
+            for (int j = 20; j <= 720; j += 100) {
+                g.drawRect(i, j, 100, 100);
+            }
+        }
     }
 
-    public void blueTile(Graphics g){
-        g.setColor(Color.BLUE);
-        g.fillRect(400,100,100,100);
-        g.fillRect(700,200,100,100);
-        g.fillRect(0,500,100,100);
-        g.fillRect(700,500,100,100);
-        g.fillRect(400,600,100,100);
+    public void greenTile(Graphics g) {
+        g.setColor(GREEN);
+        g.fillRect(600, 23, 99, 99);
+        g.fillRect(100, 122, 99, 99);
+        g.fillRect(500, 222, 99, 99);
+        g.fillRect(100, 322, 99, 99);
+        g.fillRect(300, 322, 99, 99);
+        g.fillRect(600, 422, 99, 99);
+        g.fillRect(200, 522, 99, 99);
+        g.fillRect(600, 722, 99, 99);
     }
 
-    public void yellowTile(Graphics g){
-        g.setColor(Color.YELLOW);
-        g.fillRect(0,700,100,100);
+    public void blueTile(Graphics g) {
+        g.setColor(BLUE);
+        g.fillRect(400, 122, 99, 99);
+        g.fillRect(700, 222, 99, 99);
+        g.fillRect(0, 522, 99, 99);
+        g.fillRect(700, 522, 99, 99);
+        g.fillRect(400, 622, 99, 99);
+    }
+
+    private int randTiles() {
+        Random random = new Random();
+        int rand = random.nextInt(5);
+        while (rand == 2) {
+            rand = random.nextInt(5);
+        }
+        return rand;
     }
 }
