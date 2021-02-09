c18ac067dec1dff0b5b944f3200386b455a2ea17
tihomir-dimitrov <63392523+tihomir-dimitrov@users.noreply.github.com>
2021-02-09 16:10:57 +0200

    Update GameTile.java

--- pu-oop-java-hw-4/src/Game/GameTile.java
+++ pu-oop-java-hw-4/src/Game/GameTile.java
@@ -28,34 +28,51 @@ public class GameTile extends JFrame {
             }
         }
     }
-
-    public void greenTile(Graphics g) {
-        g.setColor(GREEN);
-        g.fillRect(600, 23, 99, 99);
-        g.fillRect(100, 122, 99, 99);
-        g.fillRect(500, 222, 99, 99);
-        g.fillRect(100, 322, 99, 99);
-        g.fillRect(300, 322, 99, 99);
-        g.fillRect(600, 422, 99, 99);
-        g.fillRect(200, 522, 99, 99);
-        g.fillRect(600, 722, 99, 99);
-    }
-
-    public void blueTile(Graphics g) {
-        g.setColor(BLUE);
-        g.fillRect(400, 122, 99, 99);
-        g.fillRect(700, 222, 99, 99);
-        g.fillRect(0, 522, 99, 99);
-        g.fillRect(700, 522, 99, 99);
-        g.fillRect(400, 622, 99, 99);
-    }
-
+    
     private int randTiles() {
         Random random = new Random();
-        int rand = random.nextInt(5);
+        int rand = random.nextInt(8);
         while (rand == 2) {
-            rand = random.nextInt(5);
+            rand = random.nextInt(8);
         }
         return rand;
+    }
+
+    public void blueTile(Graphics g){
+
+        int blueTile1 = randTiles();
+        int blueTile2 = randTiles();
+        int blueTile3 = randTiles();
+        int blueTile4 = randTiles();
+        int blueTile5 = randTiles();
+
+        g.setColor(Color.BLUE);
+        g.fillRect((blueTile1*100)+20,122, 99, 99);
+        g.fillRect((blueTile2*100)+20,322, 99, 99);
+        g.fillRect((blueTile3*100)+20,422, 99, 99);
+        g.fillRect((blueTile4*100)+20,522, 99, 99);
+        g.fillRect((blueTile5*100)+20,622, 99, 99);
+    }
+
+    public void greenTile(Graphics g){
+
+        int greenTile1 = randTiles();
+        int greenTile2 = randTiles();
+        int greenTile3 = randTiles();
+        int greenTile4 = randTiles();
+        int greenTile5 = randTiles();
+        int greenTile6 = randTiles();
+        int greenTile7 = randTiles();
+        int greenTile8 = randTiles();
+
+        g.setColor(GREEN);
+        g.fillRect((greenTile1*100)+20,23,99,99);
+        g.fillRect((greenTile2*100)+20,123,99,99);
+        g.fillRect((greenTile3*100)+20,223,99,99);
+        g.fillRect((greenTile4*100)+20,323,99,99);
+        g.fillRect((greenTile5*100)+20,423,99,99);
+        g.fillRect((greenTile6*100)+20,523,99,99);
+        g.fillRect((greenTile7*100)+20,623,99,99);
+        g.fillRect((greenTile8*100)+20,723,99,99);
     }
 }
